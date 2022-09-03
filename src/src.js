fc93c5234d2ea9188aa2b24db2f00c32610ea596
Markus Fisch <mf@markusfisch.de>
2022-09-03 11:53:11 +0200

    Make stage local
    More compact and performance is not an issue here.

--- src/src.js
+++ src/src.js
@@ -20,10 +20,7 @@ const setBackground = (color) => document.documentElement.style.background = col
 		scene: scenes.opening
 	}
 
-let stage,
-	stageWidth,
-	stageHeight,
-	centerX,
+let centerX,
 	centerY,
 	bubble
 
@@ -50,17 +47,21 @@ function say(who, what) {
 	bubble.p.style.left = (cx - x) + "px"
 }
 
-function setup(name) {
-	state.scene = scenes[name]
-	state.scene.setup()
+function clear() {
+	bubble.style.display = "none"
 }
 
 function show(list) {
-	bubble.style.display = "none"
+	clear()
 	for (let key in props) {
 		props[key].style.visibility = "hidden"
 	}
 	list.forEach((o) => o.style.visibility = "visible")
+}
+
+function setup(name) {
+	state.scene = scenes[name]
+	state.scene.setup()
 }
 
 function centerScale(ratio, x, y) {
@@ -74,14 +75,14 @@ function resize() {
 	const windowWidth = window.innerWidth,
 		windowHeight = window.innerHeight,
 		min = Math.min(windowWidth, windowHeight),
-		ratio = min / 300
+		ratio = min / 300,
+		stageWidth = windowWidth / ratio,
+		stageHeight = windowHeight / ratio
 
-	stageWidth = windowWidth / ratio
-	stageHeight = windowHeight / ratio
 	centerX = stageWidth * .5
 	centerY = stageHeight * .5
 
-	const style = stage.style
+	const style = document.getElementById("S").style
 	style.width = stageWidth + "px"
 	style.height = stageHeight + "px"
 	style.transformOrigin = "top left"
@@ -92,11 +93,10 @@ function resize() {
 }
 
 window.onload = function() {
-	stage = document.getElementById("S")
 	bubble = document.getElementById("B")
 	bubble.m = document.getElementById("BM")
 	bubble.p = document.getElementById("BP")
-	;[...stage.getElementsByTagName("g")].forEach(e => props[e.id] = e)
+	;[...document.getElementsByTagName("g")].forEach(e => props[e.id] = e)
 	window.onresize = resize
 	resize()
 }
