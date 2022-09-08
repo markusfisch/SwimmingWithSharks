02a5f0d1fd51a9ac1ccc64544333404f7378b944
Markus Fisch <mf@markusfisch.de>
2022-09-08 21:29:42 +0200

    Simplify accessing and setting objects

--- src/src.js
+++ src/src.js
@@ -1,170 +1,159 @@
 "use strict"
 
 const scenes = {
-		Stern: {
-			setup: function() {
-				props.Boat.style.transform = centerScale(230, 24)
-				props.Dave.style.transform = centerScale()
-				props.Sheryl.style.transform = centerScale(-50)
-				props.Googles.style.transform = centerScale(-65, 55, .3) +
-					"rotateZ(-80deg)"
-				show([props.Boat, props.Stern,
-					props.Dave,
-					props.Sheryl, props.Watch, props.Googles
+		Stern: function() {
+			set(Boat, 230, 24)
+			set(Dave)
+			set(Sheryl, -50)
+			set(Googles, -62, 26, .3, -80)
+			show([Boat, Stern,
+				Dave,
+				Sheryl, Watch, Googles
+			])
+			Dave.onclick = function() {
+				sayLater(Dave, "That's me!")
+			}
+			Sheryl.onclick = function() {
+				sayLater([Dave, "Hi, Sheryl, you swimming?",
+					Sheryl, "None of your business, Dave.",
 				])
-				props.Googles.onclick = function() {
-					sayLater(props.Dave, "Diving googles!")
-				}
-				props.Watch.onclick = function() {
-					sayLater(props.Dave, "That's a sharp looking diving watch!")
-				}
-				props.Sheryl.onclick = function() {
-					sayLater([props.Dave, "Hi, Sheryl, you swimming?",
-						props.Sheryl, "None of your business, Dave.",
-					])
-				}
-				if (!state.greet) {
-					state.greet = 1
-					say(props.Dave, "Hello! Hello!")
-				}
+			}
+			Watch.onclick = function() {
+				sayLater(Dave, "That's a sharp looking diving watch!")
+			}
+			Googles.onclick = function() {
+				sayLater(Dave, "Diving googles!")
 			}
 		},
-		Inside: {
-			setup: function() {
-				props.Boat.style.transform = centerScale(-12, -5)
-				props.Sheryl.style.transform = centerScale(-110)
-				props.Googles.style.transform = centerScale(-125, 55, .3) +
-					"rotateZ(-80deg)"
-				props.Dave.style.transform = centerScale(-75, 10)
-				props.Skipper.style.transform = centerScale(-18, 20)
-				props.Amanda.style.transform = centerScale(45)
-				props.Bruce.style.transform = centerScale(85, 5)
-				show([props.Boat, props.Inside,
-					props.Dave,
-					props.Sheryl, props.Watch, props.Googles,
-					props.Amanda, props.Bruce,
-					props.Skipper
+		Cabin: function() {
+			set(Boat, -12, -5)
+			set(Sheryl, -110)
+			set(Googles, -122, 26, .3, -80)
+			set(Dave, -75, 10)
+			set(Skipper, -18, 20)
+			set(Amanda, 45)
+			set(Book, 35, 28, 1, -32)
+			set(Bruce, 85, 5)
+			show([Boat, Cabin,
+				Dave,
+				Sheryl, Watch, Googles,
+				Amanda, Book,
+				Bruce,
+				Skipper
+			])
+			Sheryl.onclick = function() {
+				sayLater([Dave, "What do you think, Sheryl?",
+					Sheryl, "I think it was a heart attack, Dave.",
 				])
-				props.Sheryl.onclick = function() {
-					sayLater([props.Dave, "What do you think, Sheryl?",
-						props.Sheryl, "I think it was a heart attack, Dave.",
-					])
-				}
-				props.Amanda.onclick = function() {
-					sayLater([props.Dave, "What do you think, Amanda?",
-						props.Amanda, "It may have something to do with the Triangle…",
-						props.Dave, "What Triangle?",
-						props.Amanda, "We're in the middle of the Bermuda Triangle!",
-						props.Dave, "And?",
-						props.Amanda, "There are mysterious forces…",
-					])
-				}
-				props.Bruce.onclick = function() {
-					sayLater([props.Dave, "Did you see anything, Bruce?",
-						props.Bruce, "I saw how he was looking at my wife.",
-						props.Dave, "Yeah, and this is why you killed him?",
-						props.Bruce, "I didn't kill the slacker. Must have been ill or something.",
-						props.Amanda, "Bermuda!",
-					])
-				}
-				props.Skipper.onclick = function() {
-					sayLater([props.Dave, "He looks unharmed. Apart from that little scratch on his arm.",
-						props.Bruce, "Death by scratch",
-					])
-				}
-				if (!state.discover) {
-					state.discover = 1
-					say([props.Dave, "What happened?",
-						props.Amanda, "Oh my god, he just collapsed!",
-						props.Bruce, "Maybe it's the salmon mousse?",
-						props.Amanda, "Don't be ridiculous! We all ate the salmon mousse!",
-						props.Bruce, "Maybe he was allergic?",
-						props.Amanda, "But he served it to us!",
-						props.Sheryl, "Maybe it was a heart attack then?",
-						props.Dave, "I don't know. I'm not a doctor. Let's see…",
-					])
-				}
 			}
-		},
-		Bow: {
-			setup: function() {
-				props.Boat.style.transform = centerScale(-230, 64)
-				props.Dave.style.transform = centerScale(-30, -2)
-				show([props.Boat, props.Bow,
-					props.Dave
+			Amanda.onclick = function() {
+				sayLater([Dave, "What do you think, Amanda?",
+					Amanda, "It may have something to do with the Triangle…",
+					Dave, "What Triangle?",
+					Amanda, "We're in the middle of the Bermuda Triangle!",
+					Dave, "And?",
+					Amanda, "There are mysterious forces…",
 				])
-				if (!state.bow) {
-					state.bow = 1
-					say(props.Dave, "I'm the king of the world!")
-				}
 			}
-		},
-		Steer: {
-			setup: function() {
-				props.Boat.style.transform = centerScale(20, 124)
-				props.Dave.style.transform = centerScale()
-				props.Sheryl.style.transform = centerScale(-75)
-				props.Googles.style.transform = centerScale(-90, 55, .3) +
-					"rotateZ(-80deg)"
-				show([props.Boat, props.Steer,
-					props.Dave,
-					props.Sheryl, props.Watch, props.Googles
+			Book.onclick = function() {
+				sayLater([Dave, "What's this book?",
+					Amanda, "It's about the Bermuda Triangle!",
 				])
-				say([props.Dave, "Now who's your captain?",
-					props.Sheryl, "Not you."
+			}
+			Bruce.onclick = function() {
+				sayLater([Dave, "Did you see anything, Bruce?",
+					Bruce, "I saw how he was looking at my wife.",
+					Dave, "Yeah, and this is why you killed him?",
+					Bruce, "I didn't kill the slacker. Must have been ill or something.",
+					Amanda, "Bermuda!",
+				])
+			}
+			Skipper.onclick = function() {
+				sayLater([Dave, "He looks unharmed. Apart from that little scratch on his arm.",
+					Bruce, "Death by scratch",
+				])
+			}
+			if (!state.discover) {
+				state.discover = 1
+				say([Dave, "What happened?",
+					Amanda, "Oh my god, he just collapsed!",
+					Bruce, "Maybe it's the salmon mousse?",
+					Amanda, "Don't be ridiculous! We all ate the salmon mousse!",
+					Bruce, "Maybe he was allergic?",
+					Amanda, "But he served it to us!",
+					Sheryl, "Maybe it was a heart attack then?",
+					Dave, "I don't know. I'm not a doctor. Let's see…",
 				])
 			}
 		},
-		Storage: {
-			setup: function() {
-				props.Boat.style.transform = centerScale(-200, -20)
-				props.DaveLeaning.style.transform = centerScale(-43, 10)
-				show([props.Boat, props.Storage,
-					props.DaveLeaning
-				])
-				say(props.DaveLeaning, "Hm, what do we have here?")
+		Deck: function() {
+			set(Boat, -230, 64)
+			set(Dave, -30, -2)
+			show([Boat, Deck,
+				Dave
+			])
+			if (!state.bow) {
+				state.bow = 1
+				say(Dave, "I'm the king of the world!")
 			}
 		},
-		Underwater: {
-			setup: function() {
-				props.Boat.style.transform = centerScale(250, -250)
-				props.Dave.style.transform = centerScale()
-				props.Shark.style.transform = centerScale(100, -50)
-				props.Key.style.transform = centerScale(-40, 130, .5)
-				show([props.Boat, props.Underwater,
-					props.Dave,
-					props.Shark,
-					props.Key
-				])
-				say(props.Dave, "Under the sea…")
-			}
+		Bridge: function() {
+			set(Boat, 20, 124)
+			set(Dave)
+			set(Sheryl, -75)
+			set(Googles, -87, 26, .3, -80)
+			show([Boat, Bridge,
+				Dave,
+				Sheryl, Watch, Googles
+			])
+			say([Dave, "Now who's your captain?",
+				Sheryl, "Not you."
+			])
+		},
+		Store: function() {
+			set(Boat, -200, -20)
+			set(DaveLeaning, -43, 10)
+			show([Boat, Store,
+				DaveLeaning
+			])
+			say(DaveLeaning, "Hm, what's in here?")
+		},
+		Underwater: function() {
+			set(Boat, 250, -250)
+			set(Dave)
+			set(Shark, 100, -50)
+			set(Key, -40, 130, .5)
+			show([Boat, Underwater,
+				Dave,
+				Shark,
+				Key
+			])
+			say(Dave, "Under the sea…")
 		},
 	},
-	props = {},
 	state = {
 		inventory: [],
 		scene: scenes.Stern
 	}
 
 let centerX,
-	centerY,
-	bubble
+	centerY
 
 function clear() {
-	clearTimeout(bubble.tid)
-	bubble.time = Date.now()
-	bubble.style.display = "none"
+	clearTimeout(B.tid)
+	B.time = Date.now()
+	B.style.display = "none"
 }
 
 function skip() {
-	if (Date.now() - bubble.time > 300) {
+	if (B.next && Date.now() - B.time > 300) {
 		clear()
-		bubble.next()
+		B.next()
 	}
 }
 
 function sayLater(who, what) {
-	bubble.talking || say(who, what)
+	B.talking || say(who, what)
 }
 
 function say(who, what) {
@@ -173,13 +162,13 @@ function say(who, what) {
 	what = a.shift()
 	clear()
 	// Set this for getBoundingClientRect() to work as expected.
-	bubble.style.left = "0px"
-	bubble.style.top = "0px"
-	bubble.style.display = "block"
-	bubble.m.innerHTML = what
+	B.style.left = "0px"
+	B.style.top = "0px"
+	B.style.display = "block"
+	BM.innerHTML = what
 	const whoRect = who.getBoundingClientRect(),
-		bubbleRect = bubble.getBoundingClientRect(),
-		margin = parseFloat(getComputedStyle(bubble).fontSize),
+		bubbleRect = B.getBoundingClientRect(),
+		margin = parseFloat(getComputedStyle(B).fontSize),
 		whoRectHalfWidth = whoRect.width / 2,
 		ww = window.innerWidth
 	let x = whoRect.x || whoRect.left,
@@ -188,44 +177,54 @@ function say(who, what) {
 		x = Math.min(ww - bubbleRect.width - margin,
 			Math.max(margin, x + whoRectHalfWidth - bubbleRect.width / 2))
 	}
-	bubble.style.left = x + "px"
-	bubble.style.top = ((whoRect.y || whoRect.top) -
+	B.style.left = x + "px"
+	B.style.top = ((whoRect.y || whoRect.top) -
 		bubbleRect.height - margin * 1.5) + "px"
-	bubble.p.style.left = (cx - x) + "px"
-	bubble.time = Date.now()
-	bubble.talking = 1
-	bubble.next = function() {
+	BP.style.left = (cx - x) + "px"
+	B.time = Date.now()
+	B.talking = 1
+	B.next = function() {
 		if (a.length > 0) {
 			say(a)
 		} else {
 			clear()
-			bubble.talking = 0
+			B.talking = 0
 		}
 	}
-	bubble.tid = setTimeout(bubble.next,
+	B.tid = setTimeout(B.next,
 		1000 + 200 * what.split(' ').length)
 }
 
 function show(list) {
 	clear()
-	for (let key in props) {
-		const prop = props[key]
-		prop.style.visibility = "hidden"
-		prop.onclick = null
+	for (let key in window) {
+		const e = window[key]
+		if (e && e.tagName == "g") {
+			e.style.visibility = "hidden"
+			e.onclick = null
+		}
 	}
 	list.forEach((o) => o.style.visibility = "visible")
 }
 
 function go(name) {
 	clear()
-	bubble.next = function() {}
-	bubble.talking = 0
+	B.next = function() {}
+	B.talking = 0
 	if (name == "Underwater" && !state.inventory.includes("Googles")) {
-		sayLater(props.Dave, "First I need diving googles.")
+		sayLater(Dave, "I need diving googles to see anything.")
 		return
 	}
 	state.scene = scenes[name]
-	state.scene.setup()
+	state.scene()
+}
+
+function set(e, x, y, size, deg) {
+	e.style.transformOrigin = `50px 50px`
+	e.style.transform = `translate(${
+		centerX - 50 + (x || 0)}px, ${
+		centerY - 50 + (y || 0)}px) rotateZ(${
+		deg || 0}deg) scale(${size || 1})`
 }
 
 function centerScale(x, y, ratio) {
@@ -254,19 +253,17 @@ function resize() {
 	style.transform = `scale(${ratio})`
 	style.display = "block"
 
-	state.scene.setup()
+	state.scene()
 }
 
 window.onload = function() {
-	bubble = document.getElementById("B")
-	bubble.m = document.getElementById("BM")
-	bubble.p = document.getElementById("BP")
-	;[...document.getElementsByTagName("g")].forEach(e => props[e.id] = e)
-	;["SternInside", "SternSteer", "SternBow", "SternUnderwater",
-		"SteerStern",
-		"BowStern",
-		"InsideStern", "InsideStorage",
-		"StorageInside",
+	;[...document.getElementsByTagName("g")].forEach(e => window[e.id] = e)
+	;["B", "BM", "BP"].forEach(id => window[id] = document.getElementById(id))
+	;["SternCabin", "SternBridge", "SternDeck", "SternUnderwater",
+		"BridgeStern",
+		"DeckStern",
+		"CabinStern", "CabinStore",
+		"StoreCabin",
 		"UnderwaterStern"
 	].forEach(id => document.getElementById(id).onclick = function() {
 		go(id.replace(/^[A-Z][a-z]*/, ""))
