a8de9635cea21a3b28c242f28ad5626a6eb2420a
Markus Fisch <mf@markusfisch.de>
2022-09-09 21:37:36 +0200

    Simplify setting objects and hotspots

--- src/src.js
+++ src/src.js
@@ -2,64 +2,42 @@
 
 const scenes = {
 		Stern: function() {
-			set(Boat, 230, 24)
-			set(Dave)
-			set(Sheryl, -50)
-			set(Goggles, -61, 28, .4, -100)
-			set(Watch, -39, 14, .17, 10)
-			show([Boat, Stern,
-				Dave,
-				Sheryl, Watch, Goggles
-			])
-			hot(Dave, function() {
+			set(Boat, null, 230, 24)
+			set(Dave, function() {
 				say([Dave, "That's me!"])
 			})
-			hot(Sheryl, function() {
+			set(Sheryl, function() {
 				say([Dave, "Hi, Sheryl, you swimming?",
 					Sheryl, "None of your business, Dave.",
 				])
-			})
-			hot(Watch, function() {
-				say([Dave, "That's a sharp looking diving watch!"])
-			})
-			hot(Goggles, function() {
+			}, -50)
+			set(Goggles, function() {
 				say([Dave, "Diving goggles!"])
-			})
+			}, -61, 28, .4, -100)
+			set(Watch, function() {
+				say([Dave, "That's a sharp looking diving watch!"])
+			}, -39, 14, .17, 10)
 		},
 		Cabin: function() {
-			set(Boat, -12, -5)
-			set(Sheryl, -110)
-			set(Goggles, -121, 28, .4, -100)
-			set(Watch, -99, 14, .17, 10)
-			set(Dave, -75, 10)
-			set(Skipper, -18, 20)
-			set(Amanda, 45, -5)
-			set(Book, 35, 21, 1, -20)
-			set(Bruce, 84)
-			set(Mousse, -10, -30)
-			show([Boat, Cabin,
-				Dave,
-				Sheryl, Watch, Goggles,
-				Amanda, Book,
-				Bruce,
-				Skipper,
-				Mousse
-			])
-			hot(Mousse, function() {
-				say([Dave, "The salmon mousse"])
-			})
-			hot(Sheryl, function() {
+			set(Boat, null, -12, -5)
+			set(Sheryl, function() {
 				say([Dave, "What do you think, Sheryl?",
 					Sheryl, "I think it was a heart attack, Dave.",
 				])
-			})
-			hot(Watch, function() {
-				say([Dave, "That's a sharp looking diving watch!"])
-			})
-			hot(Goggles, function() {
+			}, -110)
+			set(Goggles, function() {
 				say([Dave, "Diving goggles!"])
-			})
-			hot(Amanda, function() {
+			}, -121, 28, .4, -100)
+			set(Watch, function() {
+				say([Dave, "That's a sharp looking diving watch!"])
+			}, -99, 14, .17, 10)
+			set(Dave, null, -75, 10)
+			set(Skipper, function() {
+				say([Dave, "He looks unharmed. Apart from that little scratch on his arm.",
+					Bruce, "Death by scratch",
+				])
+			}, -18, 20)
+			set(Amanda, function() {
 				say([Dave, "What do you think, Amanda?",
 					Amanda, "It may have something to do with the Triangle…",
 					Dave, "What Triangle?",
@@ -67,28 +45,26 @@ const scenes = {
 					Dave, "And?",
 					Amanda, "There are mysterious forces…",
 				])
-			})
-			hot(Book, function() {
+			}, 45, -5)
+			set(Book, function() {
 				say([Dave, "What's that book?",
 					Amanda, "It's about the Bermuda Triangle!",
 					Amanda, "We are in the Bermuda Triangle - right now!",
 					Dave, "And why's that important?",
 					Amanda, "Because Aliens?! There may also be a gate to another dimension! Nobody knows what's going on here…",
 				])
-			})
-			hot(Bruce, function() {
+			}, 35, 21, 1, -20)
+			set(Bruce, function() {
 				say([Dave, "Did you see anything, Bruce?",
 					Bruce, "I saw how he was looking at my wife.",
 					Dave, "Yeah, and this is why you killed him?",
 					Bruce, "I didn't kill the slacker. Must have been ill or something.",
 					Amanda, "Bermuda!",
 				])
-			})
-			hot(Skipper, function() {
-				say([Dave, "He looks unharmed. Apart from that little scratch on his arm.",
-					Bruce, "Death by scratch",
-				])
-			})
+			}, 84)
+			set(Mousse, function() {
+				say([Dave, "The salmon mousse"])
+			}, -10, -30)
 			if (!state.discover) {
 				state.discover = 1
 				say([Dave, "What happened?",
@@ -103,58 +79,42 @@ const scenes = {
 			}
 		},
 		Deck: function() {
-			set(Boat, -230, 64)
-			set(Dave, -30, -2)
-			show([Boat, Deck,
-				Dave
-			])
+			set(Boat, null, -230, 64)
+			set(Dave, null, -30, -2)
 			if (!state.bow) {
 				state.bow = 1
 				say([Dave, "I'm the king of the world!"])
 			}
 		},
 		Bridge: function() {
-			set(Boat, 20, 124)
+			set(Boat, null, 20, 124)
 			set(Dave)
-			set(Sheryl, -75)
-			set(Goggles, -86, 28, .4, -100)
-			set(Watch, -64, 14, .17, 10)
-			show([Boat, Bridge,
-				Dave,
-				Sheryl, Watch, Goggles
-			])
+			set(Sheryl, null, -75)
+			set(Goggles, function() {
+				say([Dave, "Diving goggles!"])
+			}, -86, 28, .4, -100)
+			set(Watch, function() {
+				say([Dave, "That's a sharp looking diving watch!"])
+			}, -64, 14, .17, 10)
 			say([Dave, "Now who's your captain?",
 				Sheryl, "Not you.",
 			])
-			hot(Watch, function() {
-				say([Dave, "That's a sharp looking diving watch!"])
-			})
-			hot(Goggles, function() {
-				say([Dave, "Diving goggles!"])
-			})
 		},
 		Store: function() {
-			set(Boat, -200, -20)
-			set(DaveLeaning, -43, 10)
-			show([Boat, Store,
-				DaveLeaning
-			])
+			set(Boat, null, -200, -20)
+			set(DaveLeaning, null, -43, 10)
 			say([DaveLeaning, "What's in here?"])
 		},
 		Underwater: function() {
-			set(Boat, 250, -250)
-			set(DaveDiving, -70, -40)
-			set(Goggles, -70, -52, .4)
-			set(Shark, 50, 30, 2)
-			set(Key, 100, 132, .6, 30)
-			show([Boat, Underwater,
-				DaveDiving, Goggles,
-				Shark,
-				Key
-			])
+			set(Boat, null, 250, -250)
+			set(DaveDiving, null, -70, -40)
+			set(Goggles, null, -70, -52, .4)
+			set(Shark, null, 50, 30, 2)
+			set(Key, null, 100, 132, .6, 30)
 			say([DaveDiving, "Whoa!!!!!!"])
 		},
 	},
+	visibles = [],
 	state = {
 		inventory: [],
 		scene: scenes.Stern
@@ -162,10 +122,6 @@ const scenes = {
 
 let centerX,
 	centerY
-
-function hot(e, f) {
-	e.onclick = f
-}
 
 function clear() {
 	clearTimeout(B.tid)
@@ -222,8 +178,7 @@ function say(a, f, cont) {
 	B.tid = setTimeout(B.next, 1000 + 200 * what.split(' ').length)
 }
 
-function show(list) {
-	clear()
+function show(name) {
 	for (let key in window) {
 		const e = window[key]
 		if (e && e.tagName == "g") {
@@ -231,7 +186,11 @@ function show(list) {
 			e.onclick = null
 		}
 	}
-	list.forEach((o) => o.style.visibility = "visible")
+	visibles.length = 0
+	visibles.push(window[name])
+	state.scene = scenes[name]
+	state.scene()
+	visibles.forEach((o) => o.style.visibility = "visible")
 }
 
 function go(name) {
@@ -242,16 +201,17 @@ function go(name) {
 		return
 	}
 	clear()
-	state.scene = scenes[name]
-	state.scene()
+	show(name)
 }
 
-function set(e, x, y, size, deg) {
+function set(e, f, x, y, size, deg) {
 	e.style.transformOrigin = `50px 50px`
 	e.style.transform = `translate(${
 		centerX - 50 + (x || 0)}px, ${
 		centerY - 50 + (y || 0)}px) rotateZ(${
 		deg || 0}deg) scale(${size || 1})`
+	e.onclick = f
+	visibles.push(e)
 }
 
 function resize() {
@@ -272,7 +232,7 @@ function resize() {
 	style.transform = `scale(${ratio})`
 	style.display = "block"
 
-	state.scene()
+	show("Stern")
 }
 
 window.onload = function() {
