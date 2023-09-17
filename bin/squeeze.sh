d00261fbb82f23a835dfe8f797744ad56aca340b
Markus Fisch <mf@markusfisch.de>
2023-09-17 16:42:39 +0200

    Translate rgb() notation into hex colors
    Saves a few bytes.

--- bin/squeeze.sh
+++ bin/squeeze.sh
@@ -37,5 +37,25 @@ s/><\/line>/\/>/g;
 s/><\/path>/\/>/g;
 s/><\/polygon>/\/>/g;
 s/><\/polyline>/\/>/g;
-s/><\/rect>/\/>/g'
+s/><\/rect>/\/>/g' | awk '{
+	s = $0
+	for (;;) {
+		p = match(s, /rgb[()0-9, ]+/)
+		if (p == 0) {
+			print s
+			break
+		}
+		printf substr(s, 0, p - 1)
+		colors = substr(s, p, RLENGTH)
+		s = substr(s, p + RLENGTH)
+		if (split(colors, a, ",") == 3) {
+			split(a[1], b, "(")
+			split(a[3], c, ")")
+			red = b[2]
+			green = a[2]
+			blue = c[1]
+			printf "#%02x%02x%02x", red, green, blue
+		}
+	}
+}'
 done
